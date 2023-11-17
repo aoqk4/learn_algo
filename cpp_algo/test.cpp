7889049fe5a1d733580593ee18c392595bbfb460
ljs <qazplm807733@gmail.com>
2023-11-17 16:58:22 +0900

    백준 4949문

--- cpp_algo/test.cpp
+++ cpp_algo/test.cpp
@@ -1,8 +1,17 @@
-typedef struct 
-{
-    int a[10];
-} ABC;
+#include <iostream>
+
+void test();
 
 int main() {
-    
+    test();
+}
+
+void test() {
+    int a = 100;
+
+    int* b = &a;
+
+    delete b;
+
+
 }
--- rust_algo/src/q_4949/mod.rs
+++ rust_algo/src/q_4949/mod.rs
@@ -1,20 +1,6 @@
-// use std::fmt::Write;
 use std::io::stdin;
 
-// let mut output = String::new();
-// wirteln!();
-
-// let mut line = String::new();
-// stdin().read_line(&mut line).expect("wrong io");
-
-// let t = line.trim().parse::<usize>().unwrap();
-
-// let numbers: Vec<usize> = line
-// .split_whitespace()
-// .map(|num| num.parse::<usize>().unwrap())
-// .collect();
-
-fn main() {
+fn q_4949() {
     let mut sta_vec: Vec<u8> = vec![];
 
     loop {
@@ -60,15 +46,3 @@ fn main() {
         sta_vec.clear();
     }
 }
-
-fn sovle() {}
-
-// #[cfg(test)]
-// mod test {
-//     use super::*;
-
-//     #[test]
-//     fn case1() {}
-//     #[test]
-//     fn case2() {}
-// }
